883081c1faa9d636f0b5334428520aee0e14287f
benitakannout <94390863+benitakannout@users.noreply.github.com>
2022-01-05 19:08:58 +0200

    TimeSpan list included

--- Darbuotoju Projektas/AtaskaitaVisiPraejimai.cs
+++ Darbuotoju Projektas/AtaskaitaVisiPraejimai.cs
@@ -20,6 +20,41 @@ namespace Darbuotoju_Projektas
             _zmoniuRepozitorija = zmoniuRepozitorija;
         }
 
-        
+        public List<PraejimuSarasas> PraejimuSarasas(IvykiuRepozitorija ivykiuRepozitorija, ZmoniuRepozitorija zmoniuRepozitorija)
+        {
+            var zmones = zmoniuRepozitorija.Retrieve();
+            var items = new List<PraejimuSarasas>();
+
+            foreach (var zmogus in zmones)
+            {
+                var item = new PraejimuSarasas();
+                item.Vardas = zmogus.Vardas;
+                var ivykiai = ivykiuRepozitorija.RetrieveByPerson(item.Vardas);
+                
+                foreach (var ivykis in ivykiai)
+                {
+                    item.PraejimuKiekis += 1;
+                    item.PraejimuLaikas += ivykis.Trukme;
+
+                    if (ivykis.Id == 1)
+                    {
+                        item.Pietauti += 1;
+                        item.PietautiLaikas += ivykis.Trukme;
+                    }
+                    else if (ivykis.Id == 2)
+                    {
+                        item.Parukyti += 1;
+                        item.ParukytiLaikas += ivykis.Trukme;
+                    }
+                    else if (ivykis.Id == 3)
+                    {
+                        item.Tualetas += 1;
+                        item.TualetasLaikas += ivykis.Trukme;
+                    }
+                }
+            }
+            return items;
+        }
+
     }
 }
